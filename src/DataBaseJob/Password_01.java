eade767b3b45661783a2526bf54059018f69db30
nn98 <jkllhgb@gmail.com>
2025-05-19 10:27:41 +0900

    Refactor(Code) : Creage project files / Add package context

---
+++ src/DataBaseJob/Password_01.java
@@ -0,0 +1,28 @@
+package DataBaseJob;
+
+/*
+ * 제작 인원: 이재준
+ */
+public class Password_01 {
+	
+	private String userid;
+	private String password;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Password_01 select(String userid2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
